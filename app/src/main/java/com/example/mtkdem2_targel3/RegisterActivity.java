252106bdbac3b19f44eb89904025ee853746dcd9
sondoszo25 <sondos.zoabi25@gmail.com>
2023-06-16 18:24:45 +0300

    commit

--- app/src/main/java/com/example/mtkdem2_targel3/RegisterActivity.java
+++ app/src/main/java/com/example/mtkdem2_targel3/RegisterActivity.java
@@ -4,23 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView textView = findViewById(R.id.registerlink);
+        setContentView(R.layout.activity_register);
+        TextView textView = findViewById(R.id.loginlink);
         textView.setOnClickListener(v -> {
-            Intent i = new Intent(this, RegisterActivity.class);
+            Intent i = new Intent(this, MainActivity.class);
             startActivity(i);
         });
     }
-
-
-
-
 }
